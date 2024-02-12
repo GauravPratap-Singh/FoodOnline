b3e3de1b5d10ea348548e6ef5c89ef1ec17c9610
Gaurav pratap singh <1999gauravpratap@gmail.com>
2024-02-13 04:17:14 +0530

    Token verification and emailconfiguration

--- FoodOnline_Main/settings.py
+++ FoodOnline_Main/settings.py
@@ -142,3 +142,12 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 } 
+
+# Email configuration
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT',cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = 'qjrn cvky yntk fcmx'
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = ' FoodOnline Marketplace <1999thakursingh@gmail.com>'
